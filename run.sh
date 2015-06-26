95b33399f82f3d192f77c41783f153cbce213803
Michael Martin <mwm.cal@gmail.com>
2015-06-26 16:41:56 -0600

    adding run.sh for the docker goodness

---
+++ run.sh
@@ -0,0 +1,34 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+bin/grid start yarn
+
+mvn clean package
+
+mkdir -p deploy/samza
+
+tar -xvf ./target/hello-race-0.9.0-dist.tar.gz -C deploy/samza
+
+deploy/samza/bin/run-job.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path=file://$PWD/deploy/samza/config/race-bib-aggregator.properties
+
+sleep 5
+
+deploy/samza/bin/run-job.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path=file://$PWD/deploy/samza/config/race-checkpoint-checker.properties
+
+while true; do
+	#EVIL! DO NOT DO THIS EVER
+done
